48cd6f97117ac48e91b8fdcdbf4846301b7b588e
PONIYT <121174065+PONIYT@users.noreply.github.com>
2025-03-17 00:40:28 +0200

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,233 @@
+local AttackAnimations = {
+
+	["Absorb"] = {
+    	"rbxassetid://18187288231",
+	},
+
+	["Action"] = {
+    	"rbxassetid://17593571953",
+	},
+
+	["Attack"] = {
+    	"rbxassetid://12845378288",
+    	"rbxassetid://13307649187",
+    	"rbxassetid://13311696639",
+    	"rbxassetid://13837626742",
+    	"rbxassetid://13980101679",
+    	"rbxassetid://14137493381",
+    	"rbxassetid://14139806172",
+    	"rbxassetid://14379649100",
+    	"rbxassetid://14431331575",
+    	"rbxassetid://14480058780",
+    	"rbxassetid://14533902841",
+    	"rbxassetid://14566786497",
+    	"rbxassetid://14571012422",
+    	"rbxassetid://14619672243",
+    	"rbxassetid://14699431275",
+    	"rbxassetid://14706677261",
+    	"rbxassetid://14886832498",
+    	"rbxassetid://14894681018",
+    	"rbxassetid://15003188623",
+    	"rbxassetid://15134813843",
+    	"rbxassetid://15214523028",
+    	"rbxassetid://15297001570",
+    	"rbxassetid://15297971385",
+    	"rbxassetid://15399067354",
+    	"rbxassetid://15443689941",
+    	"rbxassetid://15689660145",
+    	"rbxassetid://15727553853",
+    	"rbxassetid://15761938856",
+    	"rbxassetid://17083997180",
+    	"rbxassetid://17141284782",
+    	"rbxassetid://17205391052",
+	},
+
+	["Attack(2)"] = {
+    	"rbxassetid://14139858370",
+    	"rbxassetid://17083998981",
+	},
+
+	["Attack2"] = {
+    	"rbxassetid://12845642828",
+    	"rbxassetid://13307681826",
+    	"rbxassetid://13837736752",
+    	"rbxassetid://13984524915",
+    	"rbxassetid://14137748840",
+    	"rbxassetid://14139905000",
+    	"rbxassetid://14431347126",
+    	"rbxassetid://14481523170",
+    	"rbxassetid://14534216662",
+    	"rbxassetid://14566843031",
+    	"rbxassetid://14620854834",
+    	"rbxassetid://14706694667",
+    	"rbxassetid://14886897726",
+    	"rbxassetid://14894698913",
+    	"rbxassetid://15003270957",
+    	"rbxassetid://15134955010",
+    	"rbxassetid://15214570612",
+    	"rbxassetid://15297031875",
+    	"rbxassetid://15297987460",
+    	"rbxassetid://15399312584",
+    	"rbxassetid://15689699463",
+    	"rbxassetid://15727664942",
+    	"rbxassetid://15762508824",
+    	"rbxassetid://17084000275",
+    	"rbxassetid://17141293456",
+    	"rbxassetid://17205395938",
+	},
+
+	["Attack2(2)"] = {
+    	"rbxassetid://14620865636",
+	},
+
+	["Attack3"] = {
+    	"rbxassetid://12846628030",
+    	"rbxassetid://13984806725",
+    	"rbxassetid://14137785395",
+    	"rbxassetid://14431381746",
+    	"rbxassetid://14534382398",
+    	"rbxassetid://14621851912",
+    	"rbxassetid://14706743550",
+    	"rbxassetid://14887593177",
+    	"rbxassetid://14894710799",
+    	"rbxassetid://15003435254",
+    	"rbxassetid://15147336602",
+    	"rbxassetid://15214658823",
+    	"rbxassetid://15297056707",
+    	"rbxassetid://15298084850",
+    	"rbxassetid://15689735343",
+    	"rbxassetid://15727774150",
+    	"rbxassetid://15762685047",
+    	"rbxassetid://17084001458",
+    	"rbxassetid://17141295663",
+    	"rbxassetid://17206216676",
+	},
+
+	["Attack3(2)"] = {
+    	"rbxassetid://14138256143",
+    	"rbxassetid://15297571467",
+    	"rbxassetid://15728500435",
+	},
+
+	["Attack4"] = {
+    	"rbxassetid://12855598895",
+    	"rbxassetid://13984923525",
+    	"rbxassetid://14138537118",
+    	"rbxassetid://14442168395",
+    	"rbxassetid://14534618423",
+    	"rbxassetid://14622030598",
+    	"rbxassetid://14715767169",
+    	"rbxassetid://14887780810",
+    	"rbxassetid://14894713887",
+    	"rbxassetid://15003601140",
+    	"rbxassetid://15147353462",
+    	"rbxassetid://15214698472",
+    	"rbxassetid://15297093697",
+    	"rbxassetid://15298164813",
+    	"rbxassetid://15689773756",
+    	"rbxassetid://15729003317",
+    	"rbxassetid://15763456548",
+    	"rbxassetid://17084002950",
+    	"rbxassetid://17141296967",
+    	"rbxassetid://17206221145",
+	},
+
+	["Attack4(2)"] = {
+    	"rbxassetid://14442359130",
+	},
+
+	["Attack5"] = {
+    	"rbxassetid://13984956067",
+    	"rbxassetid://14534450332",
+    	"rbxassetid://14622042246",
+    	"rbxassetid://14715871928",
+    	"rbxassetid://14888911999",
+    	"rbxassetid://15003725933",
+    	"rbxassetid://15147609038",
+    	"rbxassetid://15214852835",
+    	"rbxassetid://15297152415",
+    	"rbxassetid://15298189109",
+    	"rbxassetid://15690780832",
+    	"rbxassetid://15736174531",
+    	"rbxassetid://15763507362",
+    	"rbxassetid://17141300094",
+    	"rbxassetid://17206237036",
+	},
+
+	["Attack5(2)"] = {
+    	"rbxassetid://14888958609",
+	},
+
+	["Attack6"] = {
+    	"rbxassetid://14040077970",
+    	"rbxassetid://14535005075",
+    	"rbxassetid://14622195047",
+    	"rbxassetid://14727787593",
+    	"rbxassetid://14889087499",
+    	"rbxassetid://15003750099",
+    	"rbxassetid://15147833538",
+    	"rbxassetid://15214882684",
+    	"rbxassetid://15297351175",
+    	"rbxassetid://15298211724",
+    	"rbxassetid://15697509559",
+    	"rbxassetid://15736242384",
+    	"rbxassetid://17141302015",
+    	"rbxassetid://17206239713",
+	},
+
+	["Attack6(2)"] = {
+    	"rbxassetid://15147857441",
+    	"rbxassetid://15298233838",
+    	"rbxassetid://15736462496",
+	},
+
+	["Attack7"] = {
+    	"rbxassetid://14535761290",
+    	"rbxassetid://14680761175",
+    	"rbxassetid://14728765052",
+    	"rbxassetid://14890884041",
+    	"rbxassetid://15147881001",
+    	"rbxassetid://15215365875",
+    	"rbxassetid://15698379593",
+    	"rbxassetid://15736547487",
+    	"rbxassetid://17206241531",
+	},
+
+	["Attack7(2)"] = {
+    	"rbxassetid://14680787618",
+	},
+
+	["Attack8"] = {
+    	"rbxassetid://14729326590",
+    	"rbxassetid://15147939807",
+    	"rbxassetid://15698416537",
+    	"rbxassetid://15736740870",
+    	"rbxassetid://17206244641",
+	},
+
+	["Bash"] = {
+    	"rbxassetid://18225033102",
+    	"rbxassetid://18226362620",
+	},
+
+	["Beached"] = {
+    	"rbxassetid://14429708742",
+    	"rbxassetid://14431230740",
+	},
+
+	["Bite"] = {
+    	"rbxassetid://133712885529956",
+    	"rbxassetid://17705640667",
+    	"rbxassetid://91853744986846",
+	},
+
+	["Charge"] = {
+    	"rbxassetid://18225036551",
+    	"rbxassetid://18226364547",
+	},
+
+	["Lunge"] = {
+    	"rbxassetid://135624745187041",
+	},
+
+}
